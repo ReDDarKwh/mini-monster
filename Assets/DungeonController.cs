ddfcae9adb40e8b3daf71d85f564c05c28ddf611
William Henry <turbowill4@gmail.com>
2020-01-09 18:57:20 -0500

    Added tile sets

---
+++ Assets/DungeonController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class DungeonController : MonoBehaviour
+{
+    public Tilemap darkmap;
+    public Tile darktile;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+
+
+        darkmap.origin  = BackgroundMap.origin;
+        darkmap.size  = BackgroundMap.size;
+
+        foreach (Vector3Int p in DarkMap.cellBounds.allPositionsWithin)
+        {
+            DarkMap.SetTile(p, DarkTile);
+        }
+
+
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
